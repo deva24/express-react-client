54bf0da54171df19816b3c6cc171b0872b656241
Devarshi Hazarika <49090172+deva24@users.noreply.github.com>
2020-04-14 01:09:09 +0530

    presets and plugins arranged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Redirect, Link, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Redirect, Link, Switch, Route } from "react-router-dom"
 
 function k()
 {
@@ -8,10 +8,43 @@ function k()
         <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
         <Switch>
-            <Route path='/' exact><Redirect to='/home'/></Route>
+            <Route path='/' exact><Redirect to='/home' /></Route>
             <Route path='/home' exact>Home</Route>
             <Route path="/about">About</Route>
         </Switch>
     </Router>
 }
-export default k;
+
+class Kapp extends React.Component
+{
+    constructor()
+    {
+        super();
+
+        this.state = {
+            show: true
+        };
+    }
+
+    clicker = async () =>
+    {
+        await new Promise(res => setTimeout(() =>
+        {
+            res();
+        }, 1000))
+
+        this.setState({
+            show: !this.state.show
+        });
+    }
+
+    render()
+    {
+        return <div>
+            <button onClick={this.clicker}>Show / Hide</button>
+            {this.state.show ? <div>Show</div> : <div>Hide</div>}
+        </div>
+    }
+}
+
+export default Kapp;
